c678c13636d75fb169802a9fcc35fa98af4e37de
Valeriu Vicol <valeriu.vicol@mirriad.com>
2022-08-14 12:38:19 +0300

    add main

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"aaha/pkg"
+	"context"
+	"net/http"
+	"github.com/go-redis/redis/v8"
+	"github.com/julienschmidt/httprouter"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	log.SetLevel(log.TraceLevel)
+	ctx := context.Background()
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	rulesContainer := pkg.InitRules()
+	quotaValidator := pkg.QuotaValidator{RulesContainer: &rulesContainer}
+	redis := pkg.RedisRepository{Client: client, Ctx: ctx}
+	service := pkg.CoffeeService{Redis: &redis, Validator: &quotaValidator}
+	webserviceHandler := pkg.CoffeRequestHandler{Coffeeservice: &service}
+
+	router := httprouter.New()
+	router.GET("/coffee/:name", webserviceHandler.GetCoffe)
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
