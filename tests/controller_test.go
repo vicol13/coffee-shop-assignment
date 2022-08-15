c7b358ae16fc37717b24543182c0219194763eef
Valeriu Vicol <valeriu.vicol@mirriad.com>
2022-08-15 12:42:45 +0300

    preparing for review

--- tests/controller_test.go
+++
@@ -1,5 +0,0 @@
-package tests
-
-// would be ideal to have unite tests for rest of classes
-// but this will require more time and investigation 
-// as approaches in golang are completely new so far 
